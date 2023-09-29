e99fb648b87def7b47d6d5f00f6c42f8a9e3ba84
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-09-29 21:30:48 +0200

    test(components): Add tests for AvailabilityChip

---
+++ frontend/src/components/Chip/AvailabilityChip.test.tsx
@@ -0,0 +1,35 @@
+// eslint-disable-next-line testing-library/no-manual-cleanup
+import { render, screen, cleanup } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import { AvailabilityChip } from "@/components/Chip/AvailabilityChip";
+
+describe("AvailabilityChip has correct text for different prop types", async () => {
+	it("type: string", async () => {
+		const text = "Available";
+		render(<AvailabilityChip isAvailable={true} text={text} />);
+
+		const chip = screen.getByText(text);
+		expect(chip).toBeTruthy();
+	});
+
+	it("type: object", async () => {
+		const textObj = {
+			available: "Available",
+			unavailable: "Unavailable",
+		};
+
+		{
+			render(<AvailabilityChip isAvailable={true} text={textObj} />);
+			const chip = screen.getByText(textObj.available);
+			expect(chip).toBeTruthy();
+		}
+
+		cleanup();
+
+		{
+			render(<AvailabilityChip isAvailable={false} text={textObj} />);
+			const chip = screen.getByText(textObj.unavailable);
+			expect(chip).toBeTruthy();
+		}
+	});
+});
